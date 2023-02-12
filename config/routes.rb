d767aa2ac30250789ffc7b00d2155af0295fbe67
suisen26 <119019835+suisen26@users.noreply.github.com>
2023-02-12 05:41:59 +0000

    ユーザーと管理者のデバイスのルーティング修正と不要なデバイスコードを削除、ホスト許可

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  # ユーザー
+  # デバイスは登録とログインのみ
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
 
-  devise_for :admins
-  devise_for :users
+  # 管理者
+  # デバイスはログインのみ
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
