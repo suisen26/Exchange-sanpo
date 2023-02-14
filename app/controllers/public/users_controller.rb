e1a0405bc93786d05ac91804c8e4c33e931b50c8
suisen26 <119019835+suisen26@users.noreply.github.com>
2023-02-14 12:27:55 +0000

    プロフィール画面作成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,20 +1,28 @@
 class Public::UsersController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-  # 他人がユーザーの編集ページ遷移できないようにする 
+  # 他人がユーザーの編集と更新ページに遷移できないようにする 
   before_action :ensure_correct_user, only: [:edit, :update]
 
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+    @post = Post.new
+  end
+  
   def index
     @users = User.all
   end
   
-  def show
-    @user = User.find(params[:id])
-  end
-  
   def edit
+    @user = current_user
   end
   
   def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to user_path(current_user), notice: "ユーザーの更新が完了しました!"
+    else
+      render "edit"
   end
   
   def confirm_withdraw
