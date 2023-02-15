570847f3026ea41416f8436f1f4a732a3956e390
suisen26 <119019835+suisen26@users.noreply.github.com>
2023-02-15 04:10:32 +0000

    postモデルにActiveSrorageとValidatesなどの内容を記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :posts
   # ActiveStorageでプロフィール画像を保存する
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 1, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 200 }
 
+  # 画像が存在しない場合はno_image.jpgをActiveStorageに格納する
   def get_profile_image
-    # 画像が存在しない場合はno_image.jpgをActiveStorageに格納する
     unless profile_image.attached?
       file_path = Rails.root.join("app/assets/images/no_image.jpg")
       profile_image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
