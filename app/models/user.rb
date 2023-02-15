472b663a54577c20439f132a952b28b77b7d5235
suisen26 <119019835+suisen26@users.noreply.github.com>
2023-02-15 11:05:24 +0000

    ActiveStorage関連の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 200 }
 
   # 画像が存在しない場合はno_image.jpgをActiveStorageに格納する
-  def get_profile_image
+  def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join("app/assets/images/no_image.jpg")
       profile_image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
