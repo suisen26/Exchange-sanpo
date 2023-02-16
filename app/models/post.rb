e2c7b119b72f46a4e9a4c11f3646edd160143520
suisen26 <119019835+suisen26@users.noreply.github.com>
2023-02-16 12:32:41 +0000

    postモデルのActiveStorageにバリデーションを設定、headerリンク修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
   has_one_attached :post_image
 
   validates :text, presence: true, length: { maximum: 200 }
+  validates :post_image, presence: true
 
   def get_post_image(width, height)
     post_image.variant(resize_to_limit: [width, height]).processed
