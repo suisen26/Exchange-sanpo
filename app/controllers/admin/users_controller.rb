1f3e3bab099c4fa0bb70cd4f9cdee0f1ab8dd8df
EC2 Default User <ec2-user@ip-172-31-16-177.ap-northeast-1.compute.internal>
2023-02-19 02:16:56 +0000

    adminのusersコントローラに内容を記述、会員一覧と会員詳細画面作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,26 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+  
+  def index
+    @users = User.all
+  end
+
+  # def edit
+  # end
+
+  # def update
+  #   @customer.update(customer_params) ? (redirect_to admin_customer_path(@customer)) : (render :edit)
+  # end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
