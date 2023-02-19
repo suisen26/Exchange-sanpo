bb914ef4ee85387d3c2d9bbf50388f34ee69081f
EC2 Default User <ec2-user@ip-172-31-16-177.ap-northeast-1.compute.internal>
2023-02-19 02:22:23 +0000

    adminのpostsコントローラに内容を記述、投稿一覧と投稿詳細画面作成

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,2 +1,25 @@
 class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def show
+    @post = Post.find(params[:id])
+    @user = @post.user
+  end
+  
+  def index
+    @posts = Post.all
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to user_path(current_user), notice: "投稿を削除しました"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:text, :post_image, :genre_id)
+  end
+  
 end
