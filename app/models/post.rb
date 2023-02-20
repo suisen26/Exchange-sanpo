60fd74f03b89636791fa24f9131d9335b2b990ce
EC2 Default User <ec2-user@ip-172-31-16-177.ap-northeast-1.compute.internal>
2023-02-20 04:47:33 +0000

    新規投稿画面のレイアウト修正とフラッシュメッセージ表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,12 @@
 class Post < ApplicationRecord
-  belongs_to :user
-  belongs_to :genre
   # ActiveStorageで投稿画像を保存する
   has_one_attached :post_image
 
   validates :text, presence: true, length: { maximum: 200 }
   validates :post_image, presence: true
+  
+  belongs_to :user
+  belongs_to :genre
 
   def get_post_image(width, height)
     post_image.variant(resize_to_limit: [width, height]).processed
