a905a8d19ad81e3b91064c8c07c2cfb52ab49dea
EC2 Default User <ec2-user@ip-172-31-16-177.ap-northeast-1.compute.internal>
2023-02-20 09:07:25 +0000

    退会機能をsessionsコントローラに追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,9 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  
+  # createアクションの前に、user_stateメソッドを呼び出す
+  before_action :user_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -34,5 +37,18 @@ class Public::SessionsController < Devise::SessionsController
   def after_sign_out_path_for(resource)
     root_path
   end
-
+  
+  # 退会しているかを判断するメソッド
+  def user_state
+    # 【処理内容1】入力されたemailからアカウントを1件取得
+    @user = User.find_by(email: params[:user][:email])
+    # アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@user
+    # 【処理内容2】取得したアカウントのパスワードと入力されたパスワードが一致しているかを判別
+    if @user.valid_password?(params[:user][:password]) && @user.status
+      # 【処理内容3】falseの場合、サインインページにリダイレクトする
+      redirect_to new_user_session_path
+    end
+  end
+  
 end
