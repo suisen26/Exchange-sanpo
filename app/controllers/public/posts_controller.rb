45f04a5823e39cc1d55ce77a2d255ffaf8cfdeb2
EC2 Default User <ec2-user@ip-172-31-16-177.ap-northeast-1.compute.internal>
2023-02-21 06:43:37 +0000

    post_commentsとpostsコントローラにアクションを追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -10,6 +10,7 @@ class Public::PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @user = @post.user
+    @post_comment = PostComment.new
   end
 
   def index
