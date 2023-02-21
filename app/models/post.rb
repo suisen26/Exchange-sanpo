25693d78855327c497cf088e5f5a5403388ab196
EC2 Default User <ec2-user@ip-172-31-16-177.ap-northeast-1.compute.internal>
2023-02-21 12:06:56 +0000

    投稿一覧にコメント件数表示追加、投稿詳細画面のレイアウト修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,10 @@ class Post < ApplicationRecord
   validates :text, presence: true, length: { maximum: 200 }
   validates :post_image, presence: true
   validates :genre_id, presence: true
+  
+  # def get_post_image
+  #   (post_image.attached?) ? post_image : 'no_image.jpg'
+  # end
 
   def get_post_image(width, height)
     post_image.variant(resize_to_limit: [width, height]).processed
