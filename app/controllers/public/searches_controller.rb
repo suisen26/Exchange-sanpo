1e98e0094c8cf4f1fe1673262de6bf641d72f15b
EC2 Default User <ec2-user@ip-172-31-16-177.ap-northeast-1.compute.internal>
2023-02-22 11:21:59 +0000

    publicの検索機能完成

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,12 +1,11 @@
 class Public::SearchesController < ApplicationController
-  before_action :authenticate_user!, except: [:guest_sign_in, :show, :index]
   
   def search
     @model = params[:model]
     @content = params[:content]
     @method = params[:method]
     
-    if @model == "User"
+    if @model == "user"
       @records = User.search_for(@content, @method)
     else
       @records = Post.search_for(@content, @method)
