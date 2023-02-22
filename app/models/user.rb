159741609af724984245ceb010c0f7261f3f31bb
EC2 Default User <ec2-user@ip-172-31-16-177.ap-northeast-1.compute.internal>
2023-02-22 01:35:19 +0000

    relationshipsモデルとテーブルの作成、アソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,18 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  
+  # 自分がフォローされる(被フォロー)側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→ 自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 自分がフォローする(与フォロー)側の関係性
+  has_many :relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 与フォロー関係を通じて三洋 → 自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
   # ActiveStorageでプロフィール画像を保存する
   has_one_attached :profile_image
-
+  
   # 画像が存在しない場合はno_image.jpgをActiveStorageに格納する
   def get_profile_image(width, height)
     unless profile_image.attached?
@@ -29,6 +38,20 @@ class User < ApplicationRecord
       user.name = "ゲストユーザー"
     end
   end
-
-
+  
+  # フォローをした時の処理
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+  
+  # フォローを外す時の処理
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+  
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+  
 end
