e89406b2eb07207eeebbc654d82d1a76f6402277
EC2 Default User <ec2-user@ip-172-31-16-177.ap-northeast-1.compute.internal>
2023-02-22 08:58:50 +0000

    userモデルに記述した検索方法分岐をpostモデルに移動

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,5 +21,18 @@ class Post < ApplicationRecord
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
+  
+  # 検索方法分岐
+  def self.search_for(content, method)
+    if method == "perfect"
+      Post.where(title: content)
+    elsif method == "forward"
+      Post.where("title LIKE ?", content + "%")
+    elsif method == "backward"
+      Post.where("title LIKE ?", "%" + content)
+    else
+      Post.where("title LIKE ?", "%" + content + "%")
+    end
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -54,17 +54,6 @@ class User < ApplicationRecord
     followings.include?(user)
   end
   
-  # 検索方法分岐
-  def self.search_for(content, method)
-    if method == "perfect"
-      Post.where(title: content)
-    elsif method == "forward"
-      Post.where("title LIKE ?", content + "%")
-    elsif method == "backward"
-      Post.where("title LIKE ?", "%" + content)
-    else
-      Post.where("title LIKE ?", "%" + content + "%")
-    end
-  end
+  
   
 end
