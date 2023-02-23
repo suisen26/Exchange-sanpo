2b019169f79a7cf225b8fb04f1521e2f5bd6debd
EC2 Default User <ec2-user@ip-172-31-16-177.ap-northeast-1.compute.internal>
2023-02-23 07:46:29 +0000

    いいね一覧画面の修正

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,14 +4,14 @@ class Public::FavoritesController < ApplicationController
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: post.id)
     favorite.save
-    redirect_to request.referer
+    redirect_to request.referer, notice; "いいねしました！"
   end
   
   def destroy
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.find_by(post_id: post.id)
     favorite.destroy
-    redirect_to request.referer
+    redirect_to request.referer, notice: "いいねを外しました。"
   end
   
 end
