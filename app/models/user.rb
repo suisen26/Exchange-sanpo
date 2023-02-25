5640ca9c8d700ee0a3d8419902a1504942b3fe54
EC2 Default User <ec2-user@ip-172-31-16-177.ap-northeast-1.compute.internal>
2023-02-25 10:32:15 +0000

    ゲストユーザーにintroduction追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,7 @@ class User < ApplicationRecord
     find_or_create_by!(name: 'ゲストユーザー', email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "ゲストユーザー"
+      user.introduction = "私はゲストユーザーです。"
     end
   end
   
