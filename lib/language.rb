55d9523fa295eec77cd73f690f6675e9a5e83e18
EC2 Default User <ec2-user@ip-172-31-16-177.ap-northeast-1.compute.internal>
2023-03-05 11:55:03 +0000

    Google Cloud Natural Language APIを呼ぶためのライブラリ作成

---
+++ lib/language.rb
@@ -0,0 +1,17 @@
+require 'base64'
+require 'json'
+require 'net/https'
+
+module Language
+  class << self
+    def get_data(text)
+      # APIのURL作成
+      api_url = "https://language.googleapis.com/v1/documents:analyzeSentiment?key=#{ENV['GOOGLE_API_KEY']}"
+      # APIリクエスト用のJSONパラメータ
+      params = {
+        document: {
+          type: 'PLAIN_TEXT',
+          content: text
+        }
+      }.to_json
+      
