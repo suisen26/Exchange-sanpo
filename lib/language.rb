c0ad7804095da1b5c154f61bdfc9fb90172f170f
EC2 Default User <ec2-user@ip-172-31-16-177.ap-northeast-1.compute.internal>
2023-03-05 14:16:51 +0000

    Google Cloud Natural Language APIを呼ぶためのライブラリ更新

--- lib/language.rb
+++ lib/language.rb
@@ -14,4 +14,21 @@ module Language
           content: text
         }
       }.to_json
-      
+      # Google Cloud Natural Language APIにリクエスト
+      uri = URI.parse(api_url)
+      https = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl = true
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request['Content-Type'] = 'application/json'
+      response = https.request(request, params)
+      # APIレスポンス出力
+      response_body = JSON.parse(response.body)
+      if (error = response_body['error']).present?
+        raise error['message']
+      else
+        response_body['documentSentiment']['score']
+      end
+    
+    end
+  end
+end
